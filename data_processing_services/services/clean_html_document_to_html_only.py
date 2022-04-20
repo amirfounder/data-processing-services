8e19ca00fd54229dbdf739d9830adb7d2560109e
Amir Sharapov <amir@amirfounder.com>
2022-04-20 05:01:56 -0500

    updated and good to go

--- data_processing_services/services/clean_html_document_to_html_only.py
+++ data_processing_services/services/clean_html_document_to_html_only.py
@@ -1,3 +1,5 @@
+import threading
+import time
 from copy import copy
 from typing import Dict
 
@@ -23,42 +25,56 @@ class CleanHtmlDocumentToHtmlOnly(Base):
         self.index_repository = index_repository
         self.raw_repository = raw_repository
 
+    def run_in_thread(self, item):
+        try:
+            raw_document = self.raw_repository.get(item.document_id)
+
+            if not raw_document:
+                return
+
+            raw_document.load_contents()
+            raw_document.load_soup()
+
+            soup = copy(raw_document.soup)
+
+            for tag in soup.find_all():
+                if hasattr(tag, 'attrs') and isinstance(tag.attrs, dict):
+                    tag.attrs.clear()
+
+            for tag in soup.select('script'):
+                tag.decompose()
+
+            html_only_document = self.html_only_repository.create(id=raw_document.id)
+            html_only_document.contents = str(soup)
+            self.html_only_repository.update(html_only_document)
+
+            item.html_only_version_document_path = html_only_document.path
+            item.is_html_only_version_stored = True
+            self.index_repository.update(item)
+
+            self.successful_operations += 1
+        except Exception as e:
+            print(f'Exception occurred : {str(e)}. Document ID : {index.document_id}')
+            self.failed_operations += 1
+
     def run(self, params: Dict):
-        indices = self.index_repository.get_all_by_filter({
+        items = self.index_repository.get_all_by_filter({
             Index.is_html_only_version_stored: False
         })
+        print(f'Identified {len(items)} html documents to clean ...')
+        
+        threads = []
 
-        for index in indices:
-            try:
-                raw_document = self.raw_repository.get(index.document_id)
-        
-                if not raw_document:
-                    continue
-        
-                raw_document.load_contents()
-                raw_document.load_soup()
-        
-                soup = copy(raw_document.soup)
-        
-                for tag in soup.find_all():
-                    if hasattr(tag, 'attrs') and isinstance(tag.attrs, dict):
-                        tag.attrs.clear()
-        
-                for tag in soup.select('script'):
-                    tag.decompose()
+        for i, item in enumerate(items):
+            print(f'Running in thread ... {i}')
+            thread = threading.Thread(target=self.run_in_thread, args=(item,))
+            thread.start()
+            threads.append(thread)
+            while len(threads) >= 25:
+                time.sleep(1)
 
-                html_only_document = self.html_only_repository.create(id=raw_document.id)
-                html_only_document.contents = str(soup)
-                self.html_only_repository.update(html_only_document)
-        
-                index.html_only_version_document_path = html_only_document.path
-                index.is_html_only_version_stored = True
-                self.index_repository.update(index)
-        
-                self.successful_operations += 1
-            except Exception as e:
-                print(f'Exception occurred : {str(e)}. Document ID : {index.document_id}')
-                self.failed_operations += 1
+        for thread in threads:
+            thread.join()
 
         return {
             'status': 'DONE',
